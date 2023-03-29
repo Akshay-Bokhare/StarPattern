4db4ced88e15f0a9c51fec5d68849e2dadb7e938
Akshay Bokhare <bokhareakshay@gmail.com>
2023-03-29 13:42:21 +0530

    StarPattern

---
+++ StarPattern/StarPattern.java
@@ -0,0 +1,27 @@
+package com.Pattern.StarPattern;
+
+public class StarPattern {
+
+	public static void main(String[] args) {
+		
+	
+		
+		int i;
+		int j;
+		int row=15;
+		
+		for(i=0; i<=row; i++) {
+			
+			for(j=0; j<i; j++) {
+				
+				
+				System.out.print("*");
+			}
+			
+			System.out.println("");
+		}
+		
+		
+	}
+}
+	
